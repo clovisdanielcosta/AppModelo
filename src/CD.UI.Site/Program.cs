0876846a98e928fd41df724c483f364da6b212d5
Clovis Costa <user@DESKTOP-SMC6CNC>
2022-08-11 08:00:54 -0300

    Ajuste no projeto para iniciar

--- src/CD.UI.Site/Program.cs
+++ src/CD.UI.Site/Program.cs
@@ -1,21 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-}
-app.UseStaticFiles();
-
 app.UseRouting();
 
-app.UseAuthorization();
-
-app.MapRazorPages();
+app.MapControllerRoute("default", "{controler=Home}/action=Index/{id?}");
 
 app.Run();
