cf65a9c15f5c6657b4d5d0930af08c0773f2ffab
Clovis Costa <user@DESKTOP-SMC6CNC>
2022-08-15 19:56:56 -0300

    Passando domínio pela propriedade

--- src/CD.UI.Site/Extensions/EmailComercialTagHelper.cs
+++ src/CD.UI.Site/Extensions/EmailComercialTagHelper.cs
@@ -4,11 +4,12 @@ namespace CD.UI.Site.Extensions
 {
     public class EmailComercialTagHelper : TagHelper
     {
+        public string EmailDomain { get; set; } = "sales.com.br";
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
             var content = await output.GetChildContentAsync();
-            var target = content.GetContent() + "@" + "sales.com.br";
+            var target = content.GetContent() + "@" + EmailDomain;
             output.Attributes.SetAttribute("href", "mailto:" + target);
             output.Content.SetContent(target);
         }
--- src/CD.UI.Site/Extensions/EmailTagHelper.cs
+++ src/CD.UI.Site/Extensions/EmailTagHelper.cs
@@ -4,11 +4,12 @@ namespace CD.UI.Site.Extensions
 {
     public class EmailTagHelper : TagHelper
     {
+        public string EmailDomain { get; set; } = "cd.com.br";
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             output.TagName = "a";
             var content = await output.GetChildContentAsync();
-            var target = content.GetContent() + "@" + "cd.com.br";
+            var target = content.GetContent() + "@" + EmailDomain;
             output.Attributes.SetAttribute("href", "mailto:" + target);
             output.Content.SetContent(target);
         }
