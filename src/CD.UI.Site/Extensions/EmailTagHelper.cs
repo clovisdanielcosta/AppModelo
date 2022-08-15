c78d7d1b53b5b3906b8c89724c77d25264cdc9fa
Clovis Costa <user@DESKTOP-SMC6CNC>
2022-08-15 19:53:24 -0300

    Criados TagHelpers para email e usando Kebab case para diferenciar email comercial de institucional.

---
+++ src/CD.UI.Site/Extensions/EmailTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace CD.UI.Site.Extensions
+{
+    public class EmailTagHelper : TagHelper
+    {
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";
+            var content = await output.GetChildContentAsync();
+            var target = content.GetContent() + "@" + "cd.com.br";
+            output.Attributes.SetAttribute("href", "mailto:" + target);
+            output.Content.SetContent(target);
+        }
+    }
+}
